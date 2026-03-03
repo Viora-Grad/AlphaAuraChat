762ec427ce0bb7f155b98e0f160ef8b636ceb0be
Ebrahim Mohsen <143009815+EbrahimMo50@users.noreply.github.com>
2026-03-03 23:47:14 +0200

    Rename matched all the projects conventions

---
+++ AlphaAuraChat.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AlphaAuraChat.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlphaAuraChat.Infrastructure;
+
+internal class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public override Task<int> SaveChangesAsync(CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ AlphaAuraChat.Infrastructure/Clock/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using AlphaAuraChat.Application.Abstractions.Clock;
+
+namespace AlphaAuraChat.Infrastructure.Clock;
+
+internal class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
